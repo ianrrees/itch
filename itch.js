5f5828bf16001974d26cc1657a20f4639cab4497
Ian Rees <ian.rees@gmail.com>
2015-08-15 22:52:34 +1200

    hacky websocket echo test

--- itch.js
+++ itch.js
@@ -30,4 +30,61 @@
 
     // Register the extension
     ScratchExtensions.register('Ian\'s Sample extension', descriptor, ext);
-})({});
+
+var wsUri = "ws://echo.websocket.org/";
+var output;
+
+function init() {
+    console.log("running websocket init");
+ //   output = document.getElementById("output");
+    testWebSocket();
+}
+
+function testWebSocket() {
+    websocket = new WebSocket(wsUri);
+    websocket.onopen = function(evt) { onOpen(evt) };
+    websocket.onclose = function(evt) { onClose(evt) };
+    websocket.onmessage = function(evt) { onMessage(evt) };
+    websocket.onerror = function(evt) { onError(evt) };
+}
+
+function onOpen(evt) {
+    console.log("Connected websocket doodad");
+//    writeToScreen("CONNECTED");
+    doSend("WebSocket testatron");
+}
+
+function onClose(evt) {
+    console.log("Disconnected websocket doodad");
+//    writeToScreen("DISCONNECTED");
+}
+
+function onMessage(evt) {
+    console.log("Response is " + evt.data);
+//    writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>');
+    websocket.close();
+}
+
+function onError(evt) {
+    console.log("Error was " + evt.data);
+   // writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+}
+
+function doSend(message) {
+    console.log("Sent " + message);
+ //   writeToScreen("SENT: " + message);
+    websocket.send(message);
+}
+
+/*
+function writeToScreen(message) {
+    var pre = document.createElement("p");
+    pre.style.wordWrap = "break-word";
+    pre.innerHTML = message;
+    output.appendChild(pre);
+}*/
+
+init();
+})
+
+({});
