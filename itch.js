4697d1693ca5c8145bd4e2407f41561e84098c80
Ian Rees <ian.rees@gmail.com>
2015-08-15 21:46:30 +1200

    More-or-less copy-and-pasted example

---
+++ itch.js
@@ -0,0 +1,38 @@
+// Boilerplate
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.my_first_block = function() {
+        console.log("The block was run!");
+    };
+
+    ext.power = function(base, exponent) {
+        console.log("Power block was run!");
+        return Math.pow(base, exponent);
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+            [' ', 'my first block', 'my_first_block'],
+            // Block type, block name, function name, param1 default value, param2 default value
+            ['r', '%n ^ %n', 'power', 2, 3],
+        ]
+    };
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Sample extension', descriptor, ext);
+})({});
