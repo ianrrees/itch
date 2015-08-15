d166a45e3fdd3ed98305c70ee22883f65d4f2697
Ian Rees <ian.rees@gmail.com>
2015-08-15 23:00:47 +1200

    Silly merge screwup

--- itch.js
+++ itch.js
@@ -31,4 +31,46 @@
 
     // Register the extension
     ScratchExtensions.register('Ian\'s websocket extension', descriptor, ext);
-})({});
+
+var wsUri = "ws://echo.websocket.org/";
+var output;
+
+function init() {
+    console.log("running websocket init");
+    testWebSocket();
+}
+
+function testWebSocket() {
+    websocket = new WebSocket(wsUri);
+    websocket.onopen = function(evt) { onOpen(evt) };
+    websocket.onclose = function(evt) { onClose(evt) };
+    websocket.onmessage = function(evt) { onMessage(evt) };
+    websocket.onerror = function(evt) { onError(evt) };
+}
+
+function onOpen(evt) {
+    console.log("Connected websocket doodad");
+    doSend("WebSocket testatron");
+}
+
+function onClose(evt) {
+    console.log("Disconnected websocket doodad");
+}
+
+function onMessage(evt) {
+    console.log("Response is " + evt.data);
+    websocket.close();
+}
+
+function onError(evt) {
+    console.log("Error was " + evt.data);
+}
+
+function doSend(message) {
+    console.log("Sent " + message);
+    websocket.send(message);
+}
+
+})
+
+({});
